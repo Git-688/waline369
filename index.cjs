7d7e1ef419195bcee73ec8cd5672b41770c693a8
Git-688 <144002180+Git-688@users.noreply.github.com>
2026-05-12 22:03:06 +0800

    更新 index.cjs

--- index.cjs
+++ index.cjs
@@ -1,2 +1,7 @@
-const Waline = require('@waline/vercel');
-module.exports = Waline();
+const Application = require('@waline/vercel');
+module.exports = Application({
+  plugins: [],
+  async postSave(comment) {
+    // do what ever you want after comment saved
+  },
+});
